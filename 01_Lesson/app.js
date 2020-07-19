163ad530a2f0632d5b218894272ae3a27222a85c
jpromanonet <programming@jpromano.net>
2020-07-19 16:39:38 -0300

    Lesson 02

--- 01_Lesson/app.js
+++ 01_Lesson/app.js
@@ -1,1 +1,13 @@
-console.log("Hey Nerd!");
+// The old way to declare variables in JS
+
+var life = 100; // Our life bar wich is a NUMBER DATATYPE
+life = life - 10; // 90
+life = life / 10; // 10
+
+var name = "Jpromanonet"; // This is an STRING DATATYPE
+
+var checkout = true; // A BOOLEAN DATATYPE
+
+console.log(life);
+console.log(name);
+console.log(checkout);
