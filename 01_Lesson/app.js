614d1c8ad937cbf7000bcdd61e1bc32fd4571994
jpromanonet <programming@jpromano.net>
2020-07-19 17:01:00 -0300

    Lesson 02

--- 01_Lesson/app.js
+++ 01_Lesson/app.js
@@ -15,3 +15,17 @@ var box2 = null; //No value var
 console.log(life);
 console.log(name);
 console.log(checkout);
+
+// The new way to declare variables in JS
+
+// Const for "constant" means a variable can't be changed
+// or re-declare either, it's not allowed.
+const clife = 100;
+
+// If you want to create the variable and change it, use let, like so
+let letLife = 100;
+
+letLife = letLife - 50;
+
+console.log(clife);
+console.log(letLife);
