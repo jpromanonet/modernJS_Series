ce1b627a48af107d43bccd32211b1bc951518a44
jpromanonet <programming@jpromano.net>
2020-07-20 15:28:59 -0300

    Lesson 03

--- 03_Lesson/app.js
+++ 03_Lesson/app.js
@@ -2,12 +2,30 @@
 // Everything from global Scope is avaible in everywhere
 const name = "Nerd";
 
+////////////////////////////////////////// The old way to use functions!
 // Declare function
 // function scope is into the function but is not global
-function toUpper(text) {
-  const upperCased = text.toUpperCase();
-  console.log(upperCased);
+function toUpper0(text) {
+  const upperCased0 = text.toUpperCase();
+  console.log(upperCased0);
 }
 
 // Function invocation
-toUpper(name);
+toUpper0(name);
+
+////////////////////////////////////////// The new way to use functions!
+// 1st Way
+const toUpper1 = function (text) {
+  const upperCased1 = text.toUpperCase();
+  console.log(upperCased1);
+};
+
+toUpper1(name);
+
+// 2nd Way
+const toUpper2 = (text) => {
+  const upperCased2 = text.toUpperCase();
+  console.log(upperCased2);
+};
+
+toUpper2(name);
