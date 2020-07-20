bcc0447c2eb21b2f53ccdae47f1cbe1aaec6bc75
jpromanonet <programming@jpromano.net>
2020-07-20 15:22:51 -0300

    Lesson 03

--- 03_Lesson/app.js
+++ 03_Lesson/app.js
@@ -1,3 +1,13 @@
+// Declare variable name in the global scope
+// Everything from global Scope is avaible in everywhere
 const name = "Nerd";
 
-function upperCase() {}
+// Declare function
+// function scope is into the function but is not global
+function toUpper(text) {
+  const upperCased = text.toUpperCase();
+  console.log(upperCased);
+}
+
+// Function invocation
+toUpper(name);
