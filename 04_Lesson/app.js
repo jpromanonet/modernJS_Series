ec24901d8ba2035b7b7b1dd41e46a85820f1af95
jpromanonet <programming@jpromano.net>
2020-07-28 17:21:47 -0600

    Lesson 04

--- 04_Lesson/app.js
+++ 04_Lesson/app.js
@@ -4,5 +4,7 @@ const yourAge = 26;
 
 const name ="JP"
 
+// In JS numbers are converted to strings when concatenation occurs
+
 console.log(myAge + yourAge);
 console.log("Hello it's me " + name + " and my name is " + myAge);
